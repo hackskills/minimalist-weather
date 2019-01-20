55578dfc61c519b694e595cb533d14449c962674
Shaquille Wyan Que <shaquillewyanque@shaquilles-mbp.dyndns.rice.edu>
2019-01-20 16:51:24 -0600

    add fetching of temperature and weather condition

--- minimalist-weather/App.js
+++ minimalist-weather/App.js
@@ -4,21 +4,50 @@ import { Weather } from './components/Weather';
 import { API_KEY } from './utils/WeatherAPIKey';
 
 export default class App extends React.Component {
-  state = {
-    isLoading: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      temperature: 0,
+      weatherCondition: null,
+      error: null
+    };
+  }
+  
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error getting weather conditions'
+        });
+      }
+    );
   };
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+    .then(res => res.json())
+    .then(json => {
+      // console.log(json);
+      this.setState({
+        temperature: Math.round(json.main.temp),
+        weatherCondition: json.weather[0].main,
+        isLoading: false
+      });
+    });
+  }
 
   render() {
     const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        {isLoading ? (
-          <Text>Fetching the weather</Text>
-        ) : (
-            <View>
-              {Weather()}
-            </View>
-          )}
+        {isLoading ? <Text>Fetching the weather</Text> : <Weather weather={this.state.weatherCondition} temperature={this.state.temperature} />}
       </View>
     );
   }
@@ -27,8 +56,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
